749ae973a3dd4a5abdf4d24b6123fa69a9de76c7
TongDuyDat <92315251+TongDuyDat@users.noreply.github.com>
2024-07-09 08:49:51 +0700

    update db

--- database/__init__.py
+++ database/__init__.py
@@ -1,16 +1,11 @@
 from mongoengine import connect
+import os
 try:
-    username = 'dattongduy10'
-    password = 'TongDat1.'
-    hostname = 'cluster0.tcvgnb2.mongodb.net'
-    database_name = 'image_caption'
     print("connect db")
     #connect(host="mongodb://my_user:my_password@127.0.0.1:27017/my_db?authSource=my_db")
     connect(
-        host="mongodb+srv://dattongduy10:TongDat1.@cluster0.tcvgnb2.mongodb.net/image_caption"
+        host=os.getenv("database")
     )
     print("connect db succes")
 except:
     print("connect db error")
-    
-#mongodb+srv://dattongduy10:TongDat1.@cluster0.tcvgnb2.mongodb.net/
